440c2bb0d3089fcfe94e0fe1e98db0ebe3887c13
okatsn <okatsn@gmail.com>
2022-11-18 17:57:41 +0800

    New model twofstree

--- src/treemodels.jl
+++ src/treemodels.jl
@@ -13,3 +13,22 @@ function fstree()
 
     return mypipe
 end
+
+
+"""
+Return a composite tree model with TWO `FeatureSelector`: `selector_1` and `selector_2`.
+"""
+function twofstree()
+    selector1 = FeatureSelector()
+    selector2 = FeatureSelector()
+
+    treemodel = (@load "DecisionTreeRegressor" pkg = "DecisionTree" verbosity = 0)() # regressor type
+
+    mypipe = Pipeline(
+        selector_1 = selector1,
+        selector_2 = selector2,
+        tree = treemodel
+    )
+
+    return mypipe
+end
